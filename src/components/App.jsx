d02a67457990377e694d11b7e215d455aca5abc0
DmytroBrekalo <brekalo4@gmail.com>
2023-06-17 15:06:22 +0300

    Transactions

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import { Profile } from './Profile/Profile';
 import { Statistics } from './Statistics/Statistics';
 import { FriendList } from './FriendList/FriendList';
+import { TransactionHistory } from './Transactions/Transactions';
 
 import user from '../Props/user.json';
 import data from '../Props/data.json';
 import friends from '../Props/friends.json'
+import transactions from '../Props/transactions.json';
 
 export const App = () => {
   return (
@@ -18,6 +20,7 @@ export const App = () => {
       />
       <Statistics stats={data} title="Upload stats" />
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </div>  
   );
 };
